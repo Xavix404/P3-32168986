dfd06b0cf593e8b0bb55f879ced873fdaf90328d
Victor Misel <xavixeterno@gmail.com>
2026-01-31 01:13:57 -0400

    >:I

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/auth/Login.jsx";
+import Login from "./components/auth/AuthLogin.jsx";
 import Register from "./components/auth/Register.jsx";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
